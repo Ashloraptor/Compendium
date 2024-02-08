d2ae60b6110bdf036282e142dcea8e3210f99f64
Ash <ashloraptor@gmail.com>
2024-02-08 17:55:06 -0500

    Added CSS file and some styling, used dingbats as placeholders for the navigation icons.

---
+++ Assets/script.js
@@ -0,0 +1,3 @@
+var APIRootURL = 'https://plant.id/api/v3';
+//var APIKey = 
+var parameters = '/identification?details=common_names,url,description,taxonomy,rank,gbif_id,inaturalist_id,image,synonyms,edible_parts,watering&language={{LANGUAGE}}';
