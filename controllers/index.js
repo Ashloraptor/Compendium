0a3cdf86f03e9f3acf1250eb11edfd117998ccbc
Ash <ashloraptor@gmail.com>
2024-03-06 17:58:42 -0500

    Fixed an error in config>connections preventing connection to the server.

--- controllers/index.js
+++ controllers/index.js
@@ -2,8 +2,10 @@ const express = require('express');
 const router = express.Router();
 const plantController = require('../controllers/plantController');
 const apiRoutes = require('./api');
+const homeRoutes = require('./home-routes');
 
 // Routes for plant-related operations
+router.use('/', homeRoutes);
 router.post('/identify', plantController.identifyPlant);
 router.get('/:id', plantController.getPlantDetails);
 router.use('api', apiRoutes);
