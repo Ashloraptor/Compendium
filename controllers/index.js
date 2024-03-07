54dba984fdc3697258b6b128e89c054709148cdd
Ash <ashloraptor@gmail.com>
2024-03-06 19:26:15 -0500

    Corrected connections, can now successfully pull homepage.

--- controllers/index.js
+++ controllers/index.js
@@ -1,13 +1,13 @@
 const express = require('express');
 const router = express.Router();
-const plantController = require('../controllers/plantController');
+// const plantController = require('../controllers/plantController');
 const apiRoutes = require('./api');
 const homeRoutes = require('./home-routes');
 
 // Routes for plant-related operations
 router.use('/', homeRoutes);
-router.post('/identify', plantController.identifyPlant);
-router.get('/:id', plantController.getPlantDetails);
+// router.post('/identify', plantController.identifyPlant);
+// router.get('/:id', plantController.getPlantDetails);
 router.use('api', apiRoutes);
 
 module.exports = router;
