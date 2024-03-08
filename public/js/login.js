48227cd484c49032e1a96165bc9d442f55fd68cc
Ash <ashloraptor@gmail.com>
2024-03-08 09:27:51 -0500

    Corrected text in profile.handlebars

--- public/js/login.js
+++ public/js/login.js
@@ -63,16 +63,19 @@ const loginFormHandler = async (event) => {
 const signupFormHandler = async (event) => {
   event.preventDefault();
 
-  const name = document.querySelector('#name-signup').value.trim();
-  const email = document.querySelector('#email-signup').value.trim();
-  const password = document.querySelector('#password-signup').value.trim();
+  const username = document.querySelector('#name-signup');
+  const email = document.querySelector('#email-signup');
+  const password = document.querySelector('#password-signup');
 
-  if (name && email && password) {
-    const response = await fetch('/api/users', {
-      method: 'POST',
-      body: JSON.stringify({ name, email, password }),
-      headers: { 'Content-Type': 'application/json' },
-    });
+  const response = await fetch('/api/users', {
+    method: 'POST',
+    body: JSON.stringify({
+      username: username.value,
+      // email: email.value,
+      password: password.value,
+    }),
+    headers: { 'Content-Type': 'application/json' },
+  });
 
     if (response.ok) {
       document.location.replace('/profile');
@@ -80,7 +83,28 @@ const signupFormHandler = async (event) => {
       alert(response.statusText);
     }
   }
-};
+;
+// const signupFormHandler = async (event) => {
+//   event.preventDefault();
+
+//   const name = document.querySelector('#name-signup').value.trim();
+//   const email = document.querySelector('#email-signup').value.trim();
+//   const password = document.querySelector('#password-signup').value.trim();
+
+//   if (name && email && password) {
+//     const response = await fetch('/api/users', {
+//       method: 'POST',
+//       body: JSON.stringify({ name, email, password }),
+//       headers: { 'Content-Type': 'application/json' },
+//     });
+
+//     if (response.ok) {
+//       document.location.replace('/profile');
+//     } else {
+//       alert(response.statusText);
+//     }
+//   }
+// };
 
 document
   .querySelector('.login-form')
