ccc525997787680cf7bdd2228ae13c3e31cb144a
Ash <ashloraptor@gmail.com>
2024-03-08 15:24:33 -0500

    Corrected models index.

--- models/index.js
+++ models/index.js
@@ -1,12 +1,15 @@
-const Category = require('./Category');
+// const Category = require('./Category');
 const User = require('./User'); 
 const Plant = require('./Plant');
 
 
 
-User.hasMany(Category, {
+User.hasMany(Plant, {
     foriegn_key: 'user_id'
 });
+// User.hasMany(Category, {
+//     foriegn_key: 'user_id'
+// });
 
 Plant.belongsTo(User, {
     foreignKey: 'user_id'
